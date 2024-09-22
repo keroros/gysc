2b4264cd63d977579e35380c6cc641eaadb75661
keroros <2862478683@qq.com>
2024-09-22 21:29:53 +0800

    down

--- pi/pi_ctrl_top/QA_DIR/csrc/filelist.cu
+++ pi/pi_ctrl_top/QA_DIR/csrc/filelist.cu
@@ -1,12 +1,12 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _19238_archive_1.so
-_19238_archive_1.so : archive.0/_19238_archive_1.a
+ARCHIVE_OBJS += _5465_archive_1.so
+_5465_archive_1.so : archive.0/_5465_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_19238_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_5465_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_19238_archive_1.so $@
+	@ln -sf .//../simv.daidir//_5465_archive_1.so $@
 
 
 
